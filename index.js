5ca7638a8f6c4b95ef26a9efc05bc0bcd62eaa6f
Katie Eells <katieeells@Katies-MBP.lan>
2023-03-07 18:39:17 -0500

    added setInterval and started makig the w functional

--- index.js
+++ index.js
@@ -4,9 +4,25 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 var player = new Image();
+
 player.src = "assets/images/cat.png";
+
 var x = 0;
 var y = 0;
+var key = 0;
+
 player.onload = function () {
     ctx.drawImage(player, x, y);
 };
+
+document.onkeydown = function(e) {
+    key = e.key;
+}
+
+setInterval(function() {
+    if (key === 'w') {
+        y -= 1;
+    }
+    ctx.clearRect(0, 0, 2000, 2000);
+    ctx.drawImage(player, x, y);
+}, 5);
